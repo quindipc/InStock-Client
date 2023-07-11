a189a34347966664054a152c33a6876b1ef07f23
amrsaquib <amrsaquib@gmail.com>
2023-07-11 17:17:05 -0400

    got the html boilerplate

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+export default function Header() {
+  return (
+    <Header>
+      <div>
+        <img />
+      </div>
+      <div>
+        <div>
+          <a>Warehouses</a>
+        </div>
+        <div>
+          <a>Inventory</a>
+        </div>
+      </div>
+    </Header>
+  );
+}
