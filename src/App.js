d646d806f8d517c730a3dc3b105bc00efc869f6a
Janvi Mander <janvimander@Janvis-Air.localdomain>
2023-07-12 12:14:33 -0400

    responsiveness for tablet

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.scss';
-import WarehousePage from './pages/WarehousePage';
+import WarehousePage from './pages/warehousePage';
 import InventoryPage from './pages/InventoryPage';
 
 function App() {
