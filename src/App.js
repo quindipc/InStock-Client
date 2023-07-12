5a4f1572fcc8cee95b722c8ad95f969c1b63d130
Janvi Mander <janvimander@Janvis-MacBook-Air.local>
2023-07-12 06:55:14 -0400

    adding routes at app.js for (home, warehouse,inventory)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Router, Route } from 'react-router-dom';
 import './App.css';
+
+const Home = () => <h1>Home</h1>;
+const Warehouse = () => <h1>Warehouse</h1>;
+const Inventory = () => <h1>Inventory</h1>;
+
 
 function App() {
   return (
-   
+   <BrowserRouter>
+    <Router>
+      <Route path="/" element={Home}/>
+      <Route path="/warehouse" element={Warehouse}/>
+      <Route path="/Inventory" element={Inventory}/>
+    </Router>
+   </BrowserRouter>
   );
 }
 
