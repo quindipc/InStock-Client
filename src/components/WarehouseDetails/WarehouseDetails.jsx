f8799c5b54779e0ffde8a9b172d9855641a561b8
Chelsea Quindipan <quindipc@gmail.com>
2023-07-12 13:08:21 -0400

    Edit Warehouse Details JSX & styles

--- src/components/WarehouseDetails/WarehouseDetails.jsx
+++ src/components/WarehouseDetails/WarehouseDetails.jsx
@@ -8,7 +8,7 @@ import { Link } from "react-router-dom";
 
 // Assets
 import arrowback from "../../assets/Icons/arrow_back-24px.svg";
-import edit from "../../assets/Icons/edit-24px.svg";
+import edit from "../../assets/Icons/edit-24px-white.svg";
 // import deleteIcon from "../../assets/Icons/delete_outline-24px.svg";
 
 export default function WarehouseDetails() {
@@ -66,85 +66,92 @@ export default function WarehouseDetails() {
   // } = getInventoryItemDetails;
 
   return (
-    <section className="warehousedetails">
-      <div className="warehousedetails__container">
-        <div className="warehousedetails__container-title">
+    <section className="whdetails">
+      <div className="whdetails__container">
+        <div className="whdetails__container-title">
           {/* Link to Warehouse List Page -- need to add */}
-          <Link to="/" className="warehousedetails__back">
+          <Link to="/" className="whdetails__back">
             <img
-              className="warehousedetails__arrowback"
+              className="whdetails__arrowback"
               src={arrowback}
               alt="Arrow back icon"
             />
           </Link>
-          {/* <h1 className="warehousedetails__name">{warehouse_name}</h1> */}
-          <h1 className="warehousedetails__name">Washington</h1>
+          {/* <h1 className="whdetails__name">{warehouse_name}</h1> */}
+          <h1 className="whdetails__name">Washington</h1>
           {/* Link to Warehouse Edit Page -- need to add */}
-          <Link to="/" className="warehousedetails__edit-details">
-            <img
-              className="warehousedetails__edit-icon"
-              src={edit}
-              alt="Edit icon"
-            />
+          <Link to="/" className="whdetails__edit">
+            Edit
+            <img className="whdetails__edit-icon" src={edit} alt="Edit icon" />
           </Link>
         </div>
 
-        <div className="warehousedetails__container-info">
-          <hr className="warehousedetails__divider" />
-          <h4 className="warehousedetails__subheader">Warehouse Address:</h4>
-          {/* <p className="warehousedetails__details">{address}, {city}, {country}</p> */}
-          <p className="warehousedetails__details">address, city, country</p>
-          <h4 className="warehousedetails__subheader">Contact Name:</h4>
-          {/* <p className="warehousedetails__details">{contact_name}</p> */}
-          <p className="warehousedetails__details">contact name</p>
-          {/* <p className="warehousedetails__details">{contact_position}</p> */}
-          <p className="warehousedetails__details">contact position</p>
-          <h4 className="warehousedetails__subheader">Contact Information</h4>
-          {/* <p className="warehousedetails__details">{contact_phone}</p> */}
-          <p className="warehousedetails__details">contact phone</p>
-          {/* <p className="warehousedetails__details">{contact_email}</p> */}
-          <p className="warehousedetails__details">contact email</p>
-          <hr className="warehousedetails__divider" />
+        <hr className="whdetails__divider" />
+        <div className="whdetails__container-info">
+          <div className="whdetails__subcontainer--address">
+            <h4 className="whdetails__subheader">Warehouse Address:</h4>
+            {/* <p className="whdetails__details">{address}, {city}, {country}</p> */}
+            <p className="whdetails__details">
+              1234 Address Street, Toronto, Canada
+            </p>
+          </div>
+          <hr className="whdetails__divider--vertical" />
+          <div className="whdetails__container-contact">
+            <div className="whdetails__subcontainer--name">
+              <h4 className="whdetails__subheader">Contact Name:</h4>
+              {/* <p className="whdetails__details">{contact_name}</p> */}
+              <p className="whdetails__details">Name Name</p>
+              {/* <p className="whdetails__details">{contact_position}</p> */}
+              <p className="whdetails__details">Warehouse Manager</p>
+            </div>
+            <div className="whdetails__subcontainer--info">
+              <h4 className="whdetails__subheader">Contact Information</h4>
+              {/* <p className="whdetails__details">{contact_phone}</p> */}
+              <p className="whdetails__details">+1 (647)-123-4567 </p>
+              {/* <p className="whdetails__details">{contact_email}</p> */}
+              <p className="whdetails__details">email@email.com</p>
+            </div>
+          </div>
         </div>
+        <hr className="whdetails__divider" />
 
-        
         {/* INVENTORY LIST ITEMS */}
         {/* Need to map this over the API */}
         {/* <div className="warehousedetails__container-item"> */}
-          {/* <hr className="warehousedetails__divider" /> */}
-          {/* <h4 className="warehousedetails__subheader">Inventory Item</h4> */}
+        {/* <hr className="warehousedetails__divider" /> */}
+        {/* <h4 className="warehousedetails__subheader">Inventory Item</h4> */}
         {/* Link to the Inventory Item -- need to add */}
-          {/* <Link to="/" className="warehousedetails__inventory-item"> */}
-          {/* <p className="warehousedetails__details">{item_name}</p>  */}
-          {/* <p className="warehousedetails__details">Item Name</p> */}
-          {/* </Link> */}
-          {/* <h4 className="warehousedetails__subheader">Status</h4> */}
-          {/* <p className="warehousedetails__details">{status}</p> */}
-          {/* <p className="warehousedetails__details">Status</p> */}
-          {/* <h4 className="warehousedetails__subheader">QTY</h4> */}
-          {/* <p className="warehousedetails__details">{quantity}</p> */}
-          {/* <p className="warehousedetails__details">Quantity</p> */}
-          {/* <h4 className="warehousedetails__subheader">Category</h4> */}
-          {/* <p className="warehousedetails__details">{category}</p> */}
-          {/* <p className="warehousedetails__details">Category</p> */}
+        {/* <Link to="/" className="warehousedetails__inventory-item"> */}
+        {/* <p className="warehousedetails__details">{item_name}</p>  */}
+        {/* <p className="warehousedetails__details">Item Name</p> */}
+        {/* </Link> */}
+        {/* <h4 className="warehousedetails__subheader">Status</h4> */}
+        {/* <p className="warehousedetails__details">{status}</p> */}
+        {/* <p className="warehousedetails__details">Status</p> */}
+        {/* <h4 className="warehousedetails__subheader">QTY</h4> */}
+        {/* <p className="warehousedetails__details">{quantity}</p> */}
+        {/* <p className="warehousedetails__details">Quantity</p> */}
+        {/* <h4 className="warehousedetails__subheader">Category</h4> */}
+        {/* <p className="warehousedetails__details">{category}</p> */}
+        {/* <p className="warehousedetails__details">Category</p> */}
 
-          {/* DELETE -- may need to change this */}
-          {/* <Link to="/" className="warehousedetails__delete"> */}
-            {/* <img */}
-              {/* className="warehousedetails__delete-icon" */}
+        {/* DELETE -- may need to change this */}
+        {/* <Link to="/" className="warehousedetails__delete"> */}
+        {/* <img */}
+        {/* className="warehousedetails__delete-icon" */}
         {/* /      src={deleteIcon} */}
-              {/* alt="delete icon" */}
-            {/* /> */}
-          {/* </Link> */}
+        {/* alt="delete icon" */}
+        {/* /> */}
+        {/* </Link> */}
 
-              {/* Link to Invnetory Item Edit Page -- need to add */}
-              {/* <Link to="/" className="warehousedetails__edit-item"> */}
-            {/* <img */}
-              {/* className="warehousedetails__edit-icon" */}
-              {/* src={edit} */}
-              {/* alt="Edit icon" */}
-            {/* /> */}
-          {/* </Link> */}
+        {/* Link to Invnetory Item Edit Page -- need to add */}
+        {/* <Link to="/" className="warehousedetails__edit-item"> */}
+        {/* <img */}
+        {/* className="warehousedetails__edit-icon" */}
+        {/* src={edit} */}
+        {/* alt="Edit icon" */}
+        {/* /> */}
+        {/* </Link> */}
         {/* </div> */}
       </div>
     </section>
