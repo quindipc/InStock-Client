89fc49506ee1bc4ace4580c0933c47ec2c038846
amrsaquib <amrsaquib@gmail.com>
2023-07-12 11:21:41 -0400

    header complete

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,15 +4,15 @@ import logo from '../../assets/Logo/InStock-Logo.svg'
 export default function Header() {
   return (
     <header className='header'>
-      <div>
+      <div className='header__logo-container'>
         <img src={logo}/>
       </div>
       <div className='header__links'>
-        <div>
-          <a>Warehouses</a>
+        <div className='header__link-container header__link-container--clicked'>
+          <a className='header__link header__link--clicked'>Warehouses</a>
         </div>
-        <div>
-          <a>Inventory</a>
+        <div className='header__link-container'>
+          <a className='header__link'>Inventory</a>
         </div>
       </div>
     </header>
