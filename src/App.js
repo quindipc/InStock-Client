1ac0b61db1d34f5dfa5c829644675c28ca65d661
intrepid <tony.kp.lau@gmail.com>
2023-07-12 15:06:28 -0400

    inventory first draft

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Footer from './components/Footer/Footer';
 
 import './App.scss';
 import WarehousePage from './pages/warehousePage';
 import InventoryPage from './pages/inventoryPage';
 import Header from './components/Header/Header';
+import Inventory from './components/Inventory/Inventory'
 
 
 function App() {
   return (
    <BrowserRouter>
    <Header />
+   <Inventory />
     <Routes>
     
       <Route path="/" element={<WarehousePage />}/>
--- src/components/Inventory/Inventory.jsx
+++ src/components/Inventory/Inventory.jsx
@@ -1,12 +1,53 @@
-{/* Need to map this over the API */ }
-// import {Link} from 'react-router-dom';
-// import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../Inventory/Inventory.scss'
 
-export default function InventoryList() {
 
-return (
-  <div className="warehousedetails__container-item">
+export default function Inventory() {
 
-  </div>
-);
+
+  const [inventoryItems, setInventoryItems] = useState([ 
+
+    { id: 1, name: 'Television', category: 'Electronics', status: 'IN STOCK', quantity: 500 },
+    { id: 2, name: 'Gym Bag', category: 'Gear', status: 'OUT OF STOCK', quantity: 0 },
+    { id: 3, name: 'Hoodie', category: 'Apparel', status: 'IN STOCK', quantity: 0 },
+
+  ]);
+
+  useEffect(() => {
+    fetch('http://localhost:8080/api/inventories') 
+      .then((response) => response.json())
+      .then((data) => setInventoryItems(data))
+      .catch((error) => console.log(error));
+  }, []);
+
+
+  return (
+    <div>
+      <table className="inventory__table">
+        <thead>
+          <tr>
+            <th className="inventory__table--header">INVENTORY ITEM</th>
+            <th className="inventory__table--header">CATEGORY</th>
+            <th className="inventory__table--header">STATUS</th>
+            <th className="inventory__table--header">QUANTITY</th>
+            <th className="inventory__table--header">ACTIONS</th>
+          </tr>
+        </thead>
+        <tbody>
+          {inventoryItems.map((item) => (
+            <tr key={item.id} className="table__row">
+              <td className="table__row--name">
+                <Link to={`/item/${item.id}`}>{item.name}</Link></td>
+              <td className="table__row--category">{item.category} </td>
+              <td className="table__row--status">{item.status} </td>
+              <td className="table__row--quantity">{item.quantity} </td>
+
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+
+  );
 }
