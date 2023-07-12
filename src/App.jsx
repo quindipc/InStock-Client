5f2a5d94c8c9077e6e1e51674d3bc41887c0de5b
Janvi Mander <janvimander@Janvis-Air.localdomain>
2023-07-12 14:36:15 -0400

    final touched on warehouse list

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.scss';
-import WarehousePage from './pages/warehousePage';
+import WarehousePage from './pages/WarehousePage';
 import InventoryPage from './pages/InventoryPage';
 
 function App() {
--- src/pages/warehousePage.jsx
+++ src/pages/warehousePage.jsx
@@ -1,12 +1,14 @@
+import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer"
 import WarehouseList from "../components/WarehouseList/WarehouseList"
 
 function WarehousePage () {
     return (
-        <>
-        <WarehouseList/>
-        <Footer />
-        </>
+        <div>
+            <Header />
+            <WarehouseList/>
+            <Footer />
+        </div>
     )
 }
 
