bd07b1154f49b194e9f0f8ae9fa787b0e3857f88
Janvi Mander <janvimander@Janvis-Air.localdomain>
2023-07-13 10:14:07 -0400

    basic styling for delete warehouse modal

--- src/components/DeleteWarehouse/DeleteWarehouse.jsx
+++ src/components/DeleteWarehouse/DeleteWarehouse.jsx
@@ -3,14 +3,17 @@ import x_button from "../../assets/Icons/close-24px.svg";
 
 export default function DeleteWarehouse() {
     return(
-        <div className='delete'>
-            <img className='delete__close' src={x_button} alt="close button outline" />
-            <h1 className='delete__title'>Delete Washington warehouse? </h1>
-            <p className='delete__subtext'>Please confirm that you’d like to delete the Washington from the list of warehouses. You won’t be able to undo this action.</p>
-            <div className='delete__buttons'>
-                <button className='delete__buttons-cancel'> <h3>Cancel</h3></button>
-                <button className='delete__buttons-delete'> <h3>Delete</h3></button>
+        <div className='delete__container'>
+            <div className='delete active' id='modal'>
+                <button className='delete__close'> <img className='delete__close' src={x_button} alt="close button outline" /> </button>
+                <h1 className='delete__title'>Delete Washington warehouse? </h1>
+                <p className='delete__subtext'>Please confirm that you’d like to delete the Washington from the list of warehouses. You won’t be able to undo this action.</p>
+                <div className='delete__buttons'>
+                    <button className='delete__buttons-cancel'> <h3>Cancel</h3></button>
+                    <button className='delete__buttons-delete'> <h3>Delete</h3></button>
+                </div>
             </div>
+            <div id='overlay'></div>
         </div>
     )
 }
