6dd662115bb441e256fe24d49ad057d20f3204ef
intrepid <tony.kp.lau@gmail.com>
2023-07-13 11:58:17 -0400

    stylin for feature 11

--- src/App.js
+++ src/App.js
@@ -6,14 +6,13 @@ import './App.scss';
 import WarehousePage from './pages/warehousePage';
 import InventoryPage from './pages/inventoryPage';
 import Header from './components/Header/Header';
-import Inventory from './components/Inventory/Inventory'
 
 
 function App() {
   return (
    <BrowserRouter>
    <Header />
-   <Inventory />
+ 
     <Routes>
     
       <Route path="/" element={<WarehousePage />}/>
