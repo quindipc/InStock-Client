f6a46a6707e31ada4eed6ca90f0aae54bc9156ba
Janvi Mander <janvimander@Janvis-Air.localdomain>
2023-07-13 14:13:20 -0400

    moving the single warehouse to the correct branch

--- src/components/WarehouseList/WarehouseList.jsx
+++ src/components/WarehouseList/WarehouseList.jsx
@@ -13,38 +13,6 @@ export default function WarehouseList() {
                 <button className='warehouses__button'> <h3>+ Add New Warehouse</h3></button>
             </div>
             {/* add in the single warehouse componets here  */}
-            <div className='warehouse__item'>
-                <div className='warehouse__text'>
-                    <div className='warehouse__left'>
-                        <div className='warehouse__left-name'> 
-                            <h4 className='warehouse__titles'>WAREHOUSE</h4>
-                            <button className='warehouse__left-name-button'>
-                                <h3 className='warehouse__text-blue'> wh name</h3>
-                                <img src={chevron} alt="chevron arrow right" />
-                            </button>
-                        </div>
-                        <div className='warehouse__left-address'> 
-                            <h4 className='warehouse__titles'>ADDRESS</h4>
-                            <p>the warehouse ADDRESS</p>
-                        </div>
-                    </div>
-                    <div className='warehouse__right'>
-                        <div className='warehouse__right-contact'> 
-                            <h4 className='warehouse__titles'>CONTACT NAME</h4>
-                            <p>their name here</p>
-                        </div>
-                        <div className='warehouse__right-info'> 
-                            <h4 className='warehouse__titles'>CONTACT INFORMATION</h4>
-                            <p>their # here</p>
-                            <p>their email here</p>
-                        </div>
-                    </div>
-                </div>
-                <div className='warehouse__icons'>
-                    <button id='delete__button' className='warehouse__icons-delete'><img src={trash} alt="delete trash can" /></button>
-                    <button id='edit__button' className='warehouse__icons-edit'><img src={edit} alt="edit pencil" /></button>
-                </div>
-            </div>
         </div>
     )
 }
