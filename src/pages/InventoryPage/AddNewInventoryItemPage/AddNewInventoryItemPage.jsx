f78381b0740c562f379fb53eb919e87acb677954
Chelsea Quindipan <quindipc@gmail.com>
2023-07-14 13:08:15 -0400

    Add Inventory Pages and routes

---
+++ src/pages/InventoryPage/AddNewInventoryItemPage/AddNewInventoryItemPage.jsx
@@ -0,0 +1,14 @@
+// Components
+import Header from "../../../components/Header/Header";
+import Footer from "../../../components/Footer/Footer"
+import AddNewInventoryItemPage from "../../../components/AddNewInventoryItem/AddNewInventoryItem";
+
+export default function AddNewInventoryItemPage() {
+    return (
+        <div>
+            <Header />
+            <AddNewInventoryItemPage />
+            <Footer />
+        </div>
+    )
+}
