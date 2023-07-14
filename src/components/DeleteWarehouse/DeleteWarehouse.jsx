8a70c5b2104bc2104a7fb417b2158c69899a1539
Janvi Mander <janvimander@Janvis-Air.localdomain>
2023-07-14 09:49:35 -0400

    adding modal skeleton

--- src/components/DeleteWarehouse/DeleteWarehouse.jsx
+++ src/components/DeleteWarehouse/DeleteWarehouse.jsx
@@ -1,19 +1,19 @@
 import './DeleteWarehouse.scss';
 import x_button from "../../assets/Icons/close-24px.svg";
 
-export default function DeleteWarehouse() {
+export default function DeleteWarehouse({setOpenModal}) {
+
     return(
-        <div className='delete__container'>
+        <div className='delete__container' id='overlay'>
             <div className='delete active' id='modal'>
-                <button className='delete__close'> <img className='delete__close' src={x_button} alt="close button outline" /> </button>
+                <button className='delete__close' onClick={()=> setOpenModal(false)}> <img className='delete__close' src={x_button} alt="close button outline" /> </button>
                 <h1 className='delete__title'>Delete Washington warehouse? </h1>
                 <p className='delete__subtext'>Please confirm that you’d like to delete the Washington from the list of warehouses. You won’t be able to undo this action.</p>
                 <div className='delete__buttons'>
-                    <button className='delete__buttons-cancel'> <h3>Cancel</h3></button>
-                    <button className='delete__buttons-delete'> <h3>Delete</h3></button>
+                    <button className='delete__buttons-cancel' onClick={()=> setOpenModal(false)}> <h3>Cancel</h3></button>
+                    <button className='delete__buttons-delete' type='submit'> <h3>Delete</h3></button>
                 </div>
             </div>
-            <div id='overlay'></div>
         </div>
     )
 }
