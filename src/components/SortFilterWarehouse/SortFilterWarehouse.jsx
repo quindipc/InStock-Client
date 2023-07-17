e34defff9fec8fb2983ed0e02be3c92823c73c79
intrepid <tony.kp.lau@gmail.com>
2023-07-17 19:15:07 -0400

    modified styling v4

--- src/components/SortFilterWarehouse/SortFilterWarehouse.jsx
+++ src/components/SortFilterWarehouse/SortFilterWarehouse.jsx
@@ -4,19 +4,19 @@ import scroll from '../../assets/Icons/sort-24px.svg';
 export default function SortFilterWarehouse(){
     return(
         <div className='filter'>
-            <div className='filter__warehouse'>
+            <div className='filter__inventory'>
                 <h4>INVENTORY ITEM</h4>
                 <img src={scroll} alt="" />
             </div>
-            <div className='filter__address'>
+            <div className='filter__category'>
                 <h4>CATEGORY</h4>
                 <img src={scroll} alt="" />
             </div>
-            <div className='filter__name'>
+            <div className='filter__status'>
                 <h4>STATUS</h4>
                 <img src={scroll} alt="" />
             </div>
-            <div className='filter__info'>
+            <div className='filter__quantity'>
                 <h4>QUANTITY</h4>
                 <img src={scroll} alt="" />
             </div>
