f51b43001a8352c5db05e84fc375526b3d52c523
Janvi Mander <janvimander@Janvis-Air.localdomain>
2023-07-17 15:33:35 -0400

    row alignment done

--- src/components/SortFilter/SortFilter.jsx
+++ src/components/SortFilter/SortFilter.jsx
@@ -5,23 +5,23 @@ export default function SortFilter(){
     return(
         <div className='filter'>
             <div className='filter__warehouse'>
-                <h4>WAREHOUSE</h4>
+                <h6>WAREHOUSE</h6>
                 <img src={scroll} alt="" />
             </div>
             <div className='filter__address'>
-                <h4>ADDRESS</h4>
+                <h6>ADDRESS</h6>
                 <img src={scroll} alt="" />
             </div>
             <div className='filter__name'>
-                <h4>CONTACT NAME</h4>
+                <h6>CONTACT NAME</h6>
                 <img src={scroll} alt="" />
             </div>
             <div className='filter__info'>
-                <h4>CONTACT INFORMATION</h4>
+                <h6>CONTACT INFORMATION</h6>
                 <img src={scroll} alt="" />
             </div>
             <div className='filter__action'>
-                <h4>ACTIONS</h4>
+                <h6>ACTIONS</h6>
                 <img src={scroll} alt="" />
             </div>
 
