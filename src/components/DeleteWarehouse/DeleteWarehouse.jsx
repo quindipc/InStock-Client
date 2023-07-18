daa175642c2376292cb428a3f9445ea15e774a86
Janvi Mander <janvimander@Janvis-MacBook-Air.local>
2023-07-17 21:05:48 -0400

    added delete functionality

--- src/components/DeleteWarehouse/DeleteWarehouse.jsx
+++ src/components/DeleteWarehouse/DeleteWarehouse.jsx
@@ -1,8 +1,21 @@
 import './DeleteWarehouse.scss';
 import x_button from "../../assets/Icons/close-24px.svg";
 import React from 'react';
+import axios from 'axios';
 
 export default function DeleteWarehouse({ setOpenModal, warehouse }) {
+    const BASE_URL = "http://localhost:8080/api/warehouses";
+
+    const handleDelete = () => {
+        axios.delete(`${BASE_URL}/${warehouse.id}`)
+        .then(()=> {
+            console.log('successfully deleted warehouse');
+            setOpenModal(false);
+        })
+        .catch((error)=> {
+            console.error("Error deleting warehouse:", error);
+        });
+    };
 
     return(
         <div className='delete__container' id='overlay'>
@@ -12,7 +25,7 @@ export default function DeleteWarehouse({ setOpenModal, warehouse }) {
                 <p className='delete__subtext'>Please confirm that you’d like to delete the {warehouse.warehouse_name} from the list of warehouses. You won’t be able to undo this action.</p>
                 <div className='delete__buttons'>
                     <button className='delete__buttons-cancel' onClick={()=> setOpenModal(false)}> <h3>Cancel</h3></button>
-                    <button className='delete__buttons-delete' type='submit'> <h3>Delete</h3></button>
+                    <button className='delete__buttons-delete' onClick={handleDelete}> <h3>Delete</h3></button>
                 </div>
             </div>
         </div>
