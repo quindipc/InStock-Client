98ab6ac20721d0c634a04a254f42ae9a4379946e
Janvi Mander <janvimander@Janvis-MacBook-Air.local>
2023-07-17 21:23:25 -0400

    added in skeleton for the delete inventory modal

--- src/components/DeleteInventory/DeleteInventory.jsx
+++ src/components/DeleteInventory/DeleteInventory.jsx
@@ -7,8 +7,8 @@ export default function DeleteInventory({setOpenModal}) {
         <div className='delete__container' id='overlay'>
             <div className='delete active' id='modal'>
                 <button className='delete__close' onClick={()=> setOpenModal(false)}> <img className='delete__close' src={x_button} alt="close button outline" /> </button>
-                <h1 className='delete__title'>Delete Tv Inventory Item? </h1>
-                <p className='delete__subtext'>Please confirm that you’d like to delete the TV from the Inventory List. You won’t be able to undo this action.</p>
+                <h1 className='delete__title'>Delete {inventory.inventory_name} Inventory Item? </h1>
+                <p className='delete__subtext'>Please confirm that you’d like to delete the {inventory.inventory_name} from the Inventory List. You won’t be able to undo this action.</p>
                 <div className='delete__buttons'>
                     <button className='delete__buttons-cancel' onClick={()=> setOpenModal(false)}> <h3>Cancel</h3></button>
                     <button className='delete__buttons-delete' type='submit'> <h3>Delete</h3></button>
--- src/pages/InventoryPage/InventoryPage.jsx
+++ src/pages/InventoryPage/InventoryPage.jsx
@@ -1,16 +1,22 @@
 // Dependancies
-import React from "react";
+import React, { useState } from "react";
 
 // // Pages
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import InventoryList from "../../components/InventoryList/InventoryList"
+import DeleteInventory from "../../components/DeleteInventory/DeleteInventory";
 
 export default function InventoryPage() {
+  const [openModal, setOpenModal] = useState(false);
+  const [inventory, setInventory] = useState(null);
   return (
     <>
       <Header />
-      <InventoryList />
+      <InventoryList setOpenModal={setOpenModal} setInventory={setInventory}/>
+      {openModal && (
+        <DeleteInventory inventory={inventory} setOpenModal={setOpenModal} />
+      )}
       <Footer />
     </>
   );
